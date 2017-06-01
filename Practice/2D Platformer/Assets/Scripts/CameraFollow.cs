828dfdbe9c823384e61bd81ee677eb64105533fc
Yongjae Lee <yongjae.lee@wrl.onl>
2017-06-01 23:13:39 +0900

    Add camera bounding

--- Practice/2D Platformer/Assets/Scripts/CameraFollow.cs
+++ Practice/2D Platformer/Assets/Scripts/CameraFollow.cs
@@ -11,6 +11,11 @@ public class CameraFollow : MonoBehaviour {
 
 	public GameObject player;
 
+	public bool bounds;
+
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -21,5 +26,11 @@ public class CameraFollow : MonoBehaviour {
 		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
 		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+		if (bounds) {
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x),
+				Mathf.Clamp (transform.position.y, minCameraPos.y, maxCameraPos.y),
+				Mathf.Clamp (transform.position.z, minCameraPos.z, maxCameraPos.z));
+		}
 	}
 }
