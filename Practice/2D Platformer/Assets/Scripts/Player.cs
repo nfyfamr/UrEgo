bdccaca35c4632b29054e24c7849d12abbb140db
Yongjae Lee <yongjae.lee@wrl.onl>
2017-06-01 22:50:38 +0900

    Add double jump feature

--- Practice/2D Platformer/Assets/Scripts/Player.cs
+++ Practice/2D Platformer/Assets/Scripts/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	// Floats
 	public float maxSpeed = 3f;
 	public float speed = 50f;
 	public float jumpPower = 150f;
 
+	// Booleans
 	public bool grounded;
+	public bool canDoubleJump;
 
+	// References 
 	private Rigidbody2D rb2d;
 	private Animator anim;
 
@@ -31,8 +35,17 @@ public class Player : MonoBehaviour {
 			transform.localScale = new Vector3 (1, 1, 1);
 		}
 
-		if (Input.GetButton("Jump") && grounded) {
-			rb2d.AddForce (Vector2.up * jumpPower);
+		if (Input.GetButton("Jump")) {
+			if (grounded) {
+				rb2d.AddForce (Vector2.up * jumpPower);
+				canDoubleJump = true;
+			} else {
+				if (canDoubleJump) {
+					canDoubleJump = false;
+					rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+					rb2d.AddForce (Vector2.up * jumpPower / 1.75f);
+				}
+			}
 		}
 	}
 
