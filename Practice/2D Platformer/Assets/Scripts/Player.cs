d1cdb65f590745b0e70adb4e8a7cded4a20844c8
Yongjae Lee <yongjae.lee@wrl.onl>
2017-06-01 22:21:54 +0900

    Add ground check script
    1. Add ground check script and adapt
    
    2. Change ‘Has Exit Time’ parameter to false in all animation shift conditions.
        * In shifting animation, the parameter ‘has Exit Time’ makes fore anim to be exist until its life time.

--- Practice/2D Platformer/Assets/Scripts/Player.cs
+++ Practice/2D Platformer/Assets/Scripts/Player.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public float maxSpeed = 3f;
 	public float speed = 50f;
 	public float jumpPower = 150f;
 
 	public bool grounded;
 
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = gameObject.GetComponent<Rigidbody2D>();
+		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+		if (Input.GetAxis ("Horizontal") < -0.1f) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		if (Input.GetAxis ("Horizontal") > 0.1f) {
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
 	}
 
 	void FixedUpdate() {
 		float h = Input.GetAxis ("Horizontal");
 
+		// Moving the player
 		rb2d.AddForce ((Vector2.right * speed) * h);
+
+		// Limiting the player's max speed
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		} 
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		} 
 	}
 }
