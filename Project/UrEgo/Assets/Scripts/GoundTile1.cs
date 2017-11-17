338f7c880955f3ec9526ecb6b0981ec4970c243d
Yongjae Lee <yongjae.lee@wrl.onl>
2017-11-17 15:00:40 +0900

    Add grasses to start_grid

--- Project/UrEgo/Assets/Scripts/GoundTile1.cs
+++ Project/UrEgo/Assets/Scripts/GoundTile1.cs
@@ -9,31 +9,10 @@ public class GroundTile1 : Tile {
     [SerializeField]
     private Sprite[] sprites;
 
-
     public override void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
     {
-        int randomVal = Random.Range(0, 100);
-
-        if (randomVal < 10)
-        {
-            tileData.sprite = sprites[0];
-        }
-        else if (randomVal >= 10 && randomVal < 20)
-        {
-            tileData.sprite = sprites[1];
-        }
-        else if (randomVal >= 20 && randomVal < 80)
-        {
-            tileData.sprite = sprites[2];
-        }
-        else if (randomVal >= 80 && randomVal < 90)
-        {
-            tileData.sprite = sprites[3];
-        }
-        else if (randomVal > 90)
-        {
-            tileData.sprite = sprites[4];
-        }
+        int randomVal = Random.Range(0, 5);
+		tileData.sprite = sprites [randomVal];
     }
 
 
