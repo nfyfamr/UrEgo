874775b21653660238f2161ac623e5597b9fe9ed
nfyfamr <leeyj0218@hanmail.net>
2017-11-24 06:58:37 +0900

    player movement

---
+++ Project/UrEgo/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D m_Rigidbody;
+    private Vector3 pos;
+    private Transform tr;
+
+    void Start () {
+        pos = transform.position;
+        tr = transform;
+    }
+
+    public void moveUp(int n = 1)
+    {
+        pos += Vector3.up * n;
+        tr.position = pos;
+    }
+
+    public void moveDown(int n = 1)
+    {
+        pos += Vector3.down * n;
+        tr.position = pos;
+    }
+
+    public void moveLeft(int n = 1)
+    {
+        pos += Vector3.left * n;
+        tr.position = pos;
+    }
+
+    public void moveRight(int n = 1)
+    {
+        pos += Vector3.right * n;
+        tr.position = pos;
+    }
+
+    void Update () {
+
+    }
+}
