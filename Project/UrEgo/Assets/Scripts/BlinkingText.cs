5e678b94945f56d3937357d8dee500559af1b894
nfyfamr <leeyj0218@hanmail.net>
2017-11-23 19:43:49 +0900

    made tab anywherer to start ui

---
+++ Project/UrEgo/Assets/Scripts/BlinkingText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingText : MonoBehaviour {
+    Text text;
+    bool fadeIn;
+
+	// Use this for initialization
+	void Start () {
+        fadeIn = false;
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (fadeIn)
+        {
+            StartCoroutine(FadeTextToFullAlpha(text));
+        } else
+        {
+            StartCoroutine(FadeTextToZeroAlpha(text));
+        }
+	}
+
+    public IEnumerator FadeTextToFullAlpha(Text t)
+    {
+        t.color = new Color(t.color.r, t.color.g, t.color.b, 0.0f);
+        while (t.color.a < 1.0f)
+        {
+            t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a + (Time.deltaTime / 1.0f));
+            if (t.color.a >= 1.0f)
+            {
+                fadeIn = false;
+            }
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(Text t)
+    {
+        t.color = new Color(t.color.r, t.color.g, t.color.b, 1.0f);
+        while (t.color.a > 0.0f)
+        {
+            t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a - (Time.deltaTime / 1.0f));
+            if (t.color.a <= 0.0f)
+            {
+                fadeIn = true;
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Project/UrEgo/Assets/Scripts/DestroyAfterClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterClick : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            DestroyAndLoadControllor();
+        } else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            DestroyAndLoadControllor();
+        }
+        
+	}
+
+    public void DestroyAndLoadControllor()
+    {
+        GameObject startUI = GameObject.FindGameObjectsWithTag("StartUI")[0];
+        if (startUI != null)
+        {
+            Destroy(startUI);
+        } else
+        {
+            Debug.Log("Cannot find StartUI by tag.");
+        }
+    }
+}
